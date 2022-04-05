c057721d950e9491e37e84bb0b9408b826254d9a
Najeebullah Joya <najeebullah.joya79@gmail.com>
2022-04-05 12:41:21 +0430

    chnaged longbreak, shortbeark and pomodoro background when click event occure

--- script.js
+++ script.js
@@ -5,13 +5,27 @@ let shortbreak = document.querySelector('.shortbreak');
 let longbreak = document.querySelector('.longbreak');
 
 
+pomodoro.addEventListener('click', e => {
+    timer_show.innerHTML="25:00";
+    pomodoro.style.backgroundColor = "lightslategray";
+    shortbreak.style.backgroundColor = "rgb(59, 56, 56)";
+    longbreak.style.backgroundColor = "rgb(59, 56, 56)";
+
+});
 shortbreak.addEventListener('click', e => {
 
-    timer_show.innerHTML = "05:00"
+    timer_show.innerHTML = "05:00";
+    shortbreak.style.backgroundColor = "lightslategray";
+    pomodoro.style.backgroundColor = "rgb(59, 56, 56)";
+    longbreak.style.backgroundColor = "rgb(59, 56, 56)";
+
 });
 longbreak.addEventListener('click',e => {
-    
+
     timer_show.innerHTML="15:00"
+    longbreak.style.backgroundColor = "lightslategray";
+    shortbreak.style.backgroundColor = "rgb(59, 56, 56)";
+    pomodoro.style.backgroundColor = "rgb(59, 56, 56)";
 });
 
 
