a47a5de3e19b1fda7d8b30b31b9c39afa7b86f8a
Najeebullah Joya <najeebullah.joya79@gmail.com>
2022-04-07 08:54:25 +0430

    added timer for short break section

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ let start_btn = document.querySelector('.start-btn');
 let pomodoro = document.querySelector('.pomodoro');
 let shortbreak = document.querySelector('.shortbreak');
 let longbreak = document.querySelector('.longbreak');
+let shortbreak_minute=05;
+let shortbreak_second=59; 
 
 
 pomodoro.addEventListener('click', e => {
@@ -30,6 +32,28 @@ longbreak.addEventListener('click',e => {
 
 
 start_btn.addEventListener('click', e => {
+
+    if(timer_show.textContent == "05:00"){
+        const intr = setInterval(() => {
+            if(shortbreak_minute === 0 && shortbreak_second === 1){
+                clearInterval(intr);
+            }else{
+                shortbreak_second--;
+                if(shortbreak_second === 0){
+                    shortbreak_minute--; 
+                    shortbreak_second=59;
+                   
+                }
+                timer_show.textContent = "0"+shortbreak_minute + ":" +shortbreak_second; 
+            }
+
+                console.log(shortbreak_minute, shortbreak_second);
+        }, 1000);
+
+    }else{
+        console.log("LongBreak or Pomodoro");
+    }
+    
 
 
 });
