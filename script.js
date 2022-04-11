026e96d77084a2e1daff3b7464ab7913c8017862
Najeebullah Joya <najeebullah.joya79@gmail.com>
2022-04-11 14:49:23 +0430

    add clearInterval for pomodoro and longbreak

--- script.js
+++ script.js
@@ -4,41 +4,17 @@ let pomodoro = document.querySelector('.pomodoro');
 let shortbreak = document.querySelector('.shortbreak');
 let longbreak = document.querySelector('.longbreak');
 let shortbreak_minute;
-let shortbreak_second; 
+let shortbreak_second;
+var intr; 
 
-
-pomodoro.addEventListener('click', e => {
-    timer_show.textContent="25:00";
-    pomodoro.style.backgroundColor = "lightslategray";
-    shortbreak.style.backgroundColor = "rgb(59, 56, 56)";
-    longbreak.style.backgroundColor = "rgb(59, 56, 56)";
-
-});
-shortbreak.addEventListener('click', e => {
-
-    timer_show.innerHTML = "05:00";
-    shortbreak.style.backgroundColor = "lightslategray";
-    pomodoro.style.backgroundColor = "rgb(59, 56, 56)";
-    longbreak.style.backgroundColor = "rgb(59, 56, 56)";
-
-});
-longbreak.addEventListener('click',e => {
-
-    timer_show.innerHTML="15:00"
-    longbreak.style.backgroundColor = "lightslategray";
-    shortbreak.style.backgroundColor = "rgb(59, 56, 56)";
-    pomodoro.style.backgroundColor = "rgb(59, 56, 56)";
-});
 
 
 start_btn.addEventListener('click', e => {
-
-        console.log(e);
     if(timer_show.textContent == "05:00"){
         start_btn.textContent ="Stop";
         shortbreak_minute=4;
         shortbreak_second=59;
-        const intr = setInterval(() => {
+         intr = setInterval(() => {
             if(shortbreak_minute === 0 && shortbreak_second === 1){
                 clearInterval(intr);
             }else{
@@ -60,7 +36,35 @@ start_btn.addEventListener('click', e => {
     }else{
         console.log("LongBreak or Pomodoro");
     }
+});
+
+
+
+pomodoro.addEventListener('click', e => {
+    timer_show.textContent="25:00";
+    pomodoro.style.backgroundColor = "lightslategray";
+    shortbreak.style.backgroundColor = "rgb(59, 56, 56)";
+    longbreak.style.backgroundColor = "rgb(59, 56, 56)";
+    clearInterval(intr);
     
 
+});
+shortbreak.addEventListener('click', e => {
+    timer_show.innerHTML = "05:00";
+    shortbreak.style.backgroundColor = "lightslategray";
+    pomodoro.style.backgroundColor = "rgb(59, 56, 56)";
+    longbreak.style.backgroundColor = "rgb(59, 56, 56)";
 
 });
+longbreak.addEventListener('click',e => {
+   
+    timer_show.innerHTML="15:00";
+    longbreak.style.backgroundColor = "lightslategray";
+    shortbreak.style.backgroundColor = "rgb(59, 56, 56)";
+    pomodoro.style.backgroundColor = "rgb(59, 56, 56)";
+    clearInterval(intr);
+});
+
+
+
+
