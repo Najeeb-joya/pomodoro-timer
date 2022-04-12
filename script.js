38008fe92e5e7dad34d6b49a29a76bf334a5fedb
Najeebullah Joya <najeebullah.joya79@gmail.com>
2022-04-12 10:26:46 +0430

    feat: add flag variable and use it to move between pomodoro, shortbreak and longbreak tab automatically when time reach to 00:00

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ let longbreak = document.querySelector('.longbreak');
 let minute;
 let second;
 var intr; 
+var flag;
 
 
 start_btn.addEventListener('click', e => {
@@ -17,17 +18,47 @@ start_btn.addEventListener('click', e => {
     if(timer_show.textContent === "25:00"){
         minute=24;
         second=59;
+        flag="pomodoro";
     }if(timer_show.textContent === "15:00"){
         minute= 14; 
         second =59;
+        flag="longbreak";
     } if(timer_show.textContent === "05:00"){
         minute=4;
         second=59;
+        flag="shortbreak";
     }
 
     intr = setInterval(() => {
         if(minute === 0 && second === 1){
             clearInterval(intr);
+
+            if(flag =="pomodoro"){
+                timer_show.innerHTML = "05:00";
+                shortbreak.style.backgroundColor = "lightslategray";
+                pomodoro.style.backgroundColor = "rgb(59, 56, 56)";
+                longbreak.style.backgroundColor = "rgb(59, 56, 56)";
+                stop_btn.style.display="none";
+                 pause_btn.style.display="none";
+
+            }
+            if(flag === "shortbreak"){  
+                timer_show.innerHTML="15:00";
+                longbreak.style.backgroundColor = "lightslategray";
+                shortbreak.style.backgroundColor = "rgb(59, 56, 56)";
+                pomodoro.style.backgroundColor = "rgb(59, 56, 56)";
+                stop_btn.style.display="none";
+                pause_btn.style.display="none";
+            }
+            if(flag === "longbreak"){
+                timer_show.textContent="25:00";
+                pomodoro.style.backgroundColor = "lightslategray";
+                shortbreak.style.backgroundColor = "rgb(59, 56, 56)";
+                longbreak.style.backgroundColor = "rgb(59, 56, 56)";
+                stop_btn.style.display="none";
+                pause_btn.style.display="none";
+            }
+
         }else{
             second--;
             if(second === 0){
